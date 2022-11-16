75d007a52793aabf0240b79c802bfff63200e171
alexa <reddosakura>
2022-11-16 21:11:45 +0300

    [commit] practical task #4

--- Practical_Task_4/task #5.py
+++ Practical_Task_4/task #5.py
@@ -1,3 +1,22 @@
 import random
 
 
+def generatesign():
+    alphabet = list('абвгдеёжзиклмнопрстуфхцчшщыэюя')
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    signs = [f'{random.choice(alphabet).title()}'
+             f'{random.choice(alphabet).title()}'
+             f'{random.choice(alphabet).title()}'
+             f'{random.choice(numbers)}'
+             f'{random.choice(numbers)}'
+             f'{random.choice(numbers)}',
+             f'{random.choice(numbers)}'
+             f'{random.choice(numbers)}'
+             f'{random.choice(numbers)}'
+             f'{random.choice(numbers)}'
+             f'{random.choice(alphabet).title()}'
+             f'{random.choice(alphabet).title()}'
+             f'{random.choice(alphabet).title()}']
+    return random.choice(signs)
+
+print(generatesign())
